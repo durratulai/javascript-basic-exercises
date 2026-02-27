889af78c2bc3b54b49c478046cca5b57f652f9fd
durratulai <durralay@gmail.com>
2026-02-28 05:03:47 +0700

    Create calculator.js

---
+++ calculator.js
@@ -0,0 +1,10 @@
+// Simple Calculator
+
+function calculate(num1, num2) {
+    console.log("Addition:", num1 + num2);
+    console.log("Subtraction:", num1 - num2);
+    console.log("Multiplication:", num1 * num2);
+    console.log("Division:", num1 / num2);
+}
+
+calculate(10, 5);
