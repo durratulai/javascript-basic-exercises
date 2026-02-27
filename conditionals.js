aaa79d0fd57b55fc5c6fb12a831694411ec7771b
durratulai <durralay@gmail.com>
2026-02-28 05:10:13 +0700

    Create conditionals.js

---
+++ conditionals.js
@@ -0,0 +1,11 @@
+// Conditionals Example
+
+let score = 85;
+
+if (score >= 80) {
+    console.log("Grade: A");
+} else if (score >= 70) {
+    console.log("Grade: B");
+} else {
+    console.log("Grade: C");
+}
